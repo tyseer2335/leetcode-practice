cd27ae4fefacb31d58e1aca6858428c1f9385fab
unknown <tyseer2334@gmail.com>
2026-01-11 19:20:45 -0500

    Added some questions

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,4 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:  
-        ...
-        
+        # Use sets since it gets rid of duplicates
+        return len(nums) != len(set(nums)) 
+
+    # Time Complexity O(n)
+    # Space Complexity O(1) 
+    # Where n is number of elements in nums
