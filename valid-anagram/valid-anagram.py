66a68c981ca4ddf22ee0b90e7ad06051b4ccf819
unknown <tyseer2334@gmail.com>
2026-01-12 23:19:17 -0500

    Add valid anagram

---
+++ valid-anagram/valid-anagram.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool: 
+
+        # Define 2 hashmaps that we will keep track of letters 
+        # and corresponding counts
+        h1 = {} 
+        h2 = {} 
+
+        # Loop through strings and count
+        for i in s: 
+            if i not in h1: 
+                h1[i] = 1 
+            else: 
+                h1[i] += 1  
+
+        # Same thing ....
+        for k in t: 
+            if k not in h2: 
+                h2[k] = 1 
+            else: 
+                h2[k] += 1  
+
+        # If they are the same than we must have an anagram else its not
+        return h1 == h2
+                
+        # Time Complexity O(n)
+        # Space Complexity O(n)
+        # Where n is len(s) + len(t)
