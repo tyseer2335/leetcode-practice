b02fde5a40739089fffda239b42b184749c78aee
unknown <tyseer2334@gmail.com>
2026-01-13 21:45:44 -0500

    Add language notes

---
+++ language-specific-notes/C#/notes.cs
@@ -0,0 +1,27 @@
+// -- IMPORT  STATEMENTS --
+using System; // import math in python
+using System.Collections.Generic; // from collections import ... this imports List, Dictionary, Stack ect
+using System.Linq;
+
+
+// -- Hello World -- 
+Console.WriteLine("Hello, World!");
+
+
+// -- Data Types -- 
+int x = 5;   
+
+string s = "Hello, World!";   
+char c = 'A';
+
+float f = 1.67F; // Need to cast this to a float since compiler thinks its a double by default
+long y = 5000000000L; // Same here but for a int
+double d = 3.14159;  
+decimal dec = 19.99M; // Decimal
+bool b = true; 
+
+// Note that: byte → short → int → long → float → double → decimal 
+
+// We can use var to dynamically infer the type
+var name = "Tyseer"; // type inference
+
